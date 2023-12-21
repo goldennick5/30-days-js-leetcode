2883473fba32d4edb32686bbd655fe988eba3c1f
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-21 23:39:20 +0600

    redun task

--- memoize.js
+++ memoize.js
@@ -15,6 +15,18 @@ function memoize(fn) {
   }
 }
 
+//another solution using Map
+function memoize(fn) {
+  const cached = new Map()
+  return function(...args) {
+      const key = JSON.stringify(args);
+      if(cached.has(key)) {
+          return cached.get(key);
+      }
+      cached.set(key, fn(...args));
+      return cached.get(key);
+  }
+}
 
 /** 
 * let callCount = 0;
