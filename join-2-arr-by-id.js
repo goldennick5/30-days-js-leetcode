7b5b33ff8fcffb6d15885a8ed6f876af04763d15
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-23 22:47:06 +0600

    .

--- join-2-arr-by-id.js
+++ join-2-arr-by-id.js
@@ -7,25 +7,16 @@
 //val = { id: 1, x: 1 }
 
 var join = function (arr1, arr2) {
-    //merging to arrays
-    let concattedArr = arr1.concat(arr2);
-    //creating res obj to store future result values
+    let concattedArr = [...arr1, ...arr2];
     let result = {};
-
-    //looping through concattedArr
-    concattedArr.forEach((item) => {
-        //getting keys, in other words id's
-        let id = item.id;
-
-        //if id is not present in result obj
-        if(!result[id]) {
-            //creating key with id, and value where spreading object (copying)
-            result[id] = {...item};
+  
+    concattedArr.forEach(val => {
+        let key = val.id;
+        if(!result[key]) {
+            result[key] = {...val};
         } else {
-            //if id is already present in result obj, we are rewriting prev values with new, and also adding additional not already presented values to our obj
-            result[id] = {...result[id], ...item};
+            result[key] = {...result[key], ...val};
         }
-    })
-    //finally we return result obj as an array that stores values
+    });
     return Object.values(result);
-};
+  };
